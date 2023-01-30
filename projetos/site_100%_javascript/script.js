2aee9714d15ceb199118192c2e39b3fe4774a990
Gustavo Nascimento <123gustaf321@gmail.com>
2023-01-30 02:17:53 -0300

    update - site 100% javascript

--- projetos/site_100%_javascript/script.js
+++ projetos/site_100%_javascript/script.js
@@ -1,3 +1,8 @@
+const titulo = []
+const subTitulo = []
+const p = []
+const section = []
+
 // Modificações no Header:
 
 document.head.innerHTML += '<link rel="preconnect" href="https://fonts.googleapis.com">'
@@ -36,13 +41,13 @@ logo.style.maxHeight = '40px'
 
 // Header page name
 
-const titulo = document.createElement('h1')
-header.appendChild(titulo)
-titulo.style.textAlign = 'center'
-titulo.style.fontSize = '1.5em'
-titulo.style.margin = '0 20px 0 20px'
+titulo[0] = document.createElement('h1')
+header.appendChild(titulo[0])
+titulo[0].style.textAlign = 'center'
+titulo[0].style.fontSize = '1.5em'
+titulo[0].style.margin = '0 20px 0 20px'
 
-titulo.innerHTML = 'Site criado em 100% em JavaScript'
+titulo[0].innerHTML = 'Site criado em 100% em JavaScript'
 
 // Header voltar
 
@@ -57,9 +62,124 @@ link.style.fontSize = '1.2em'
 link.style.fontWeight = '600'
 link.style.color = '#1F3541'
 
-// titulo 02
+// Main
 
-const titulo2 = document.createElement('h2')
-document.body.appendChild(titulo2)
-titulo2.innerHTML = 'Projeto com objetivo de  Fixar Conhecimentos Básicos de JavaScript'
-titulo2.style.textAlign = 'center'
+const main = document.createElement('main')
+document.body.appendChild(main)
+main.style.display = 'flex'
+main.style.flexDirection = 'column'
+main.style.alignSelf = 'center'
+
+// Titulo 02
+
+titulo[1] = document.createElement('h1')
+main.appendChild(titulo[1])
+titulo[1].innerHTML = 'Projeto com objetivo de  Fixar Conhecimentos Básicos de JavaScript'
+titulo[1].style.textAlign = 'center'
+titulo[1].style.fontSize = '2em'
+
+// section comentário
+
+section[0] = document.createElement('section')
+main.appendChild(section[0])
+
+subTitulo[0] = document.createElement('h2')
+section[0].appendChild(subTitulo[0])
+subTitulo[0].innerHTML = 'Crie um Comentário: (Em desenvolvimento)'
+subTitulo[0].style.fontSize = '1.6em'
+
+// Form
+
+const label = []
+const input = []
+
+const form = document.createElement('form')
+section[0].appendChild(form)
+
+label[0] = document.createElement('p')
+form.appendChild(label[0])
+label[0].innerHTML = 'Título:'
+
+input[0] = document.createElement('input')
+form.appendChild(input[0])
+input[0].setAttribute('type', 'text')
+input[0].style.width = '300px'
+input[0].style.padding = '5px'
+input[0].style.fontSize = '1.1em'
+
+
+label[1] = document.createElement('p')
+form.appendChild(label[1])
+label[1].innerHTML = 'Comentário:'
+
+input[1] = document.createElement('textarea')
+form.appendChild(input[1])
+input[1].setAttribute('type', 'text')
+input[1].style.width = '60%'
+input[1].style.height = '150px'
+input[1].style.padding = '5px'
+input[1].style.fontSize = '1.1em'
+
+let br = document.createElement('br')
+form.appendChild(br)
+
+input[2] = document.createElement('input')
+form.appendChild(input[2])
+input[2].setAttribute('type', 'button')
+input[2].setAttribute('value', 'Comentar')
+input[2].setAttribute('onclick', 'comentar()')
+
+let hr = document.createElement('hr')
+form.appendChild(hr)
+
+//section comentários
+
+section[1] = document.createElement('section')
+section[0].appendChild(section[1])
+
+for(let i in label){
+    label[i].style.margin = '25px 0 0px 0'
+    label[i].style.fontSize = '1.3em'
+}
+// Comentário
+
+function criarComent(t, c){
+    const criarComent = []
+    const ti = []
+    
+    criarComent[0] = document.createElement('section')
+    section[1].appendChild(criarComent[0])
+
+    ti[0] = document.createElement('h3')
+    criarComent[0].appendChild(ti[0])
+    ti[0].innerHTML = t
+
+    p[0] = document.createElement('p')
+    criarComent[0].appendChild(p[0])
+    p[0].innerHTML = c
+}
+
+function comentar(){
+    const coment = {titulo: '', comentario: ''}
+
+    coment.titulo = input[0].value
+    coment.comentario = input[1].value
+
+    if(coment.titulo.length == 0 || coment.comentario.length == 0){
+        window.alert('[ERRO] Informações Faltando')
+    }
+    else{
+        criarComent(input[0].value, input[1].value)
+    }
+
+}
+
+// section style
+
+for(let i in section){
+    section[i].style.border = '1px solid #aaa'
+    section[i].style.maxWidth = '100%'
+    section[i].style.padding = '10px'
+    section[i].style.borderRadius = '10px'
+
+}
